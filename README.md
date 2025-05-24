8a94a432cf7e1e1ac8d19311e6b6b42474c9e226
Ricky <ricky@monster-puppet.com>
2025-05-24 20:48:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GameJammy
+
+Developed with Unreal Engine 5
---
+++ Source/GameJammy/GameJammyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameJammyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameJammyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameJammyGameMode();
+};
+
+
+
